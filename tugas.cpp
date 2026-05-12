5342ab266662062a0d2caad486c97f4fc1cb16ce
prinses-racwa <rajwaamelia@gmail.com>
2026-05-12 20:04:31 +0700

    menambahkn class user

--- tugas.cpp
+++ tugas.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class user {
+protected:
+    static int globalId;
+    int id;
+    string nama;
+    string email;
+
+public:
+    user(string pNama, string pEmail) {
+        id = generateId();
+        nama = pNama;
+        email = pEmail;
+    }
+
+    int generateId() {
+        return ++globalId;
+    }
+};
